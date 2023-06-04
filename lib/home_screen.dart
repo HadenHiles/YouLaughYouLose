61e77d499bff21e97f3f4ee8c4699cd662ccff27
HadenHiles <hadenhiles@gmail.com>
2023-06-04 15:44:40 -0400

    Fix invalid constructor key variables

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors_in_immutables
+
 import 'package:flutter/material.dart';
 import 'package:ylyl/play_game.dart';
 import 'package:ylyl/services/game_service.dart';
